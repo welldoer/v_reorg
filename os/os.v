00e5089f5bec68c4bed2a544de7e7ddfb314ef86
welldoer <welldoer@263.net>
2019-10-19 21:44:24 +0800

    using 10 commits from 7ff0e48 to 8b3802d on 6/27

--- os/os.v
+++ os/os.v
@@ -449,6 +449,11 @@ pub fn write_file(path, text string) {
 	f.close()
 }
 
+pub fn clear() {
+	C.printf('\x1b[2J')
+	C.printf('\x1b[H')
+}
+
 fn on_segfault(f voidptr) {
 	$if windows {
 		return
@@ -462,4 +467,3 @@ fn on_segfault(f voidptr) {
 		# sigaction(SIGSEGV, &sa, 0);
 	}
 }
-
