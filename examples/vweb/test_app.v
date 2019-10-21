692c452019e1437ca57f46ac38db85d88d85d215
welldoer <welldoer@263.net>
2019-10-21 11:09:45 +0800

    using 19 commits from acd28fa to 7918a79 on 7/31

---
+++ examples/vweb/test_app.v
@@ -0,0 +1,29 @@
+module main
+
+import vweb
+
+const (
+	Port = 8082
+)
+
+struct App {
+pub mut:
+        vweb vweb.Context // TODO embed
+}
+
+fn main() {
+        println('Running vweb test on http://localhost:$Port')
+        vweb.run<App>(Port)
+}
+
+pub fn (app mut App) init() {
+	app.vweb.handle_static('.')
+}
+
+pub fn (app mut App) json_endpoint() {
+        app.vweb.json('{"a": 3}')
+}
+
+pub fn (app mut App) index() {
+        app.vweb.text('hello world')
+}
